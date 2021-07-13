24255c9e9acc299ff1716957c181a0e8447d947b
jjg2362 <wornjs2362@gmail.com>
2021-07-13 14:41:47 +0900

    [컴포넌트 수정]atoms/Image, atoms/P > 데이터 없을 시 기본 데이터 출력하도록 수정

--- src/components/atoms/Image/index.jsx
+++ src/components/atoms/Image/index.jsx
@@ -3,7 +3,12 @@ import classNames from "classnames";
 
 import styles from "./style.module.css";
 
-const Image = ({ className, src, width = "auto", height = "auto" }) => {
+const Image = ({
+  className,
+  src = "/images/logo.png",
+  width = "auto",
+  height = "auto",
+}) => {
   const classProps = classNames(className, styles.default);
   const styleProps = {
     width,
--- src/components/atoms/P/index.jsx
+++ src/components/atoms/P/index.jsx
@@ -3,7 +3,13 @@ import classNames from "classnames";
 
 import styles from "./style.module.css";
 
-const P = ({ className, text, lineHeight = 1.2, size = 16, weight = 400 }) => {
+const P = ({
+  className,
+  text = "text",
+  lineHeight = 1.2,
+  size = 16,
+  weight = 400,
+}) => {
   const classProps = classNames(styles.default, className);
   const styleProps = {
     fontSize: size,
